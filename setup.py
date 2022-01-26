f38cde7e10070bab5ffd884359b97d5d66aef7c8
codybuell <cody@codybuell.com>
2022-01-25 20:59:27 -0500

    Read in requirements file and change method

--- setup.py
+++ setup.py
@@ -2,21 +2,23 @@
 Setup for lookatme.contrib.grapheasy
 """
 
-import pathlib
+import os
 
 from setuptools import setup, find_namespace_packages
 
-# The directory containing this file
-HERE = pathlib.Path(__file__).parent
+req_path = os.path.join(os.path.dirname(__file__), "requirements.txt")
+with open(req_path, "r") as f:
+    required = f.read().splitlines()
 
-# The text of the README file
-README = (HERE / "README.md").read_text()
+readme_path = os.path.join(os.path.dirname(__file__), "README.md")
+with open(readme_path, "r") as f:
+    readme = f.read()
 
 setup(
     name="lookatme.contrib.grapheasy",
     version="0.0.1",
     description="Adds a grapheasy code block type",
-    lond_description=README,
+    lond_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/codybuell/lookatme.contrib.grapheasy",
     author="Cody Buell",
@@ -33,6 +35,7 @@ setup(
         "Topic :: Software Development :: Documentation",
     ],
     python_requires=">=3.9",
+    install_requires=required,
     packages=find_namespace_packages(include=["lookatme.*"]),
     install_requires=[],
 )
